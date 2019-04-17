ed5e9373b7eaf3c70aa33c822caa8df01b0cc093
Sudarshan Mondal <sudarshaana@gmail.com>
2019-04-17 21:05:50 +0600

    int log, error log, log with tag added

--- logprint/src/main/java/xyz/cruxlab/logprint/SLog.java
+++ logprint/src/main/java/xyz/cruxlab/logprint/SLog.java
@@ -3,9 +3,50 @@ package xyz.cruxlab.logprint;
 import android.util.Log;
 
 public class SLog {
-    private static String TAG = "LogPrint";
+    public static String TAG = "SLog -->";
 
     public static void d(String message) {
         Log.d(TAG, message);
+    }
+
+    public static void d(int message) {
+        Log.d(TAG, message + "");
+    }
+
+    public static void d(float message) {
+        Log.d(TAG, message + "");
+    }
+
+    public static void d(double message) {
+        Log.d(TAG, message + "");
+    }
+
+    // error log
+
+    public static void e(int message) {
+        Log.e(TAG, message + "");
+    }
+
+    public static void e(String message) {
+        Log.e(TAG, message);
+    }
+
+
+    // with tag
+
+    public static void d(String TAG, String message) {
+        Log.d(TAG, message);
+    }
+
+    public static void d(String TAG, int message) {
+        Log.d(TAG, message + "");
+    }
+
+    public static void e(String TAG, int message) {
+        Log.e(TAG, message + "");
+    }
+
+    public static void e(String TAG, String message) {
+        Log.e(TAG, message);
     }
 }
